2941951effef1963b3a4fc08f68f06fcf2724348
atharvupadhyay08 <upadhyayatharv008@gmail.com>
2025-05-03 19:38:12 +0000

    conversion of datatypes

---
+++ 01_basics/03datatypes.js
@@ -0,0 +1,55 @@
+
+// let score=33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber =Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+number
+number
+number
+33
+*/
+
+// let score="33a"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber =Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// output
+// string
+// string
+// number
+// NaN
+
+//  let score=null
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+//  let valueInNumber =Number(score)
+
+//  console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// object
+// object
+// number
+// 0
+
+//score= true->1 /false->0   undefined k liye ->NaN  string->NaN
+
+let isLoggedIn = undefined
+
+let isBooleanLoggedIn = Boolean(isLoggedIn); //Number,String,Boolean
+console.log(isBooleanLoggedIn)   // 1-> true, 0->false, false->false, true-> true, "atharv"->true, ""->false,null->false,undefined->false
