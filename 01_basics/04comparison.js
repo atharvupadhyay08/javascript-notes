f2351364cdbf5822c3ab6ed33d51568c6e954700
atharvupadhyay08 <upadhyayatharv008@gmail.com>
2025-05-10 18:15:24 +0000

    comparisonsandoperators

---
+++ 01_basics/04comparison.js
@@ -0,0 +1,30 @@
+let value =3
+let negvalue=-value
+console.log(negvalue);
+ 
+// console.log(2+2)
+// console.log(3-2)
+// console.log(2**2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2=" atharv"
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2)
+console.log(2+"1")
+console.log("1"+2+2)
+console.log(1+2+"2")//ecma script to primitive
+
+console.log(+true); //true+ error
+console.log(+"");
+
+let gamecounter =100;
+gamecounter++;
+console.log(gamecounter)
+
+// let num1,num2,num3
+// num1=num2=num3=2+2;  bad practise
