fd80294041e1be989e9553f32b0dec27ac339037
atharvupadhyay08 <upadhyayatharv008@gmail.com>
2025-05-29 10:34:40 +0000

    new ones

---
+++ 01_basics/nums.js
@@ -0,0 +1,11 @@
+const score =400
+console.log(score)
+
+const balance=new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const len = 123.8934;
+console.log(len.toPrecision(3))
---
+++ 01_basics/string.js
@@ -0,0 +1,30 @@
+const name ="Atharv "
+const repoCount = 50
+
+//console.log(name + repoCount +" Value")
+// console.log(`this is my name ${name}this is my repo count${repoCount}`) 
+// // string intrepolation
+// const gameName = new String('atharvpb')
+
+// console.log(gameName[7]);
+// //console.log(gameName.__proto__)
+
+// //console.log(gameName.blink()) or .length()/.Toupercase / .tolowercase etc
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('r'))
+
+// const newString = gameName.substring(0,8);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7,5);
+// console.log(anotherString)
+
+ const AnewStrings = '  atharvupadhyay  '
+ console.log(AnewStrings)
+ console.log(AnewStrings.trim())
+
+ const url= "https://aharv000.com/atharv%30upadhyay"
+
+console.log(url .replace('%30','-'))
+console.log(url .includes('bhavya'))
+console.log(url.split('/'))
