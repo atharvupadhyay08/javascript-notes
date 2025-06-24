4b847525036352b80922d9fa07d3bff7abb9f8b7
Atharv upadhyay <upadhyayatharv008@gmail.con>
2025-06-24 22:40:48 +0530

    done

---
+++ 01_basics/array2.js
@@ -0,0 +1,14 @@
+//arrays2
+const marvel_heros =["thor","spiderman","ironman"];
+const dc_heros =["superman","flash","batman"];
+
+// marvel_heros.push(dc_heros) // isse arrays merge nahi hote hai 
+// console.log(marvel_heros);//issme dc wala array bhi aagaya as whole  another element  
+// console.log(marvel_heros[3][1]) //to traverse an elemnet in an array within an array
+
+
+// console.log(marvel_heros);
+// console.log(dc_heros);
+marvel_heros.concat(dc_heros);
+console.log(marvel_heros);
+// console.log(dc_heros);
--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,3 +1,2 @@
-marvel_heros.push(dc_heros) // isse arrays merge nahi hote hai 
-// console.log(marvel_heros);//issme dc wala array bhi aagaya as whole  another element  
-// console.log(marvel_heros[3][1]) //to traverse an elemnet in an array within an array
+console.log(marvel_heros);
+// console.lo
