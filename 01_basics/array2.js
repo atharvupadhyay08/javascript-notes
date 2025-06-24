f6ccbd2d3ef6127882c771e5ad7e91be8881bd33
Atharv upadhyay <upadhyayatharv008@gmail.con>
2025-06-24 23:02:21 +0530

    done

--- 01_basics/array2.js
+++ 01_basics/array2.js
@@ -9,6 +9,23 @@ const dc_heros =["superman","flash","batman"];
 
 // console.log(marvel_heros);
 // console.log(dc_heros);
-marvel_heros.concat(dc_heros);
-console.log(marvel_heros);
+// const newArr =marvel_heros.concat(dc_heros);//concat m naya array banana zaruri h
+// console.log(newArr);
 // console.log(dc_heros);
+
+// const all_new_heros =[...marvel_heros,...dc_heros]//spreading
+// console.log(all_new_heros)
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
+
+console.log(Array.isArray("atharv")) // bataega array h ya nahi 
+console.log(Array.from("atharv")) // array m convert kardega
+console.log(Array.from({name :"atharv"}));//yr empty array dega if it cant convert somting into array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3)); //array banane m helpful
--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,2 +1,3 @@
-console.log(marvel_heros);
-// console.lo
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// conso
