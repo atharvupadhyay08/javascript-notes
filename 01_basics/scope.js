bc7cbed063f395deb4ddb7f30d47f605917d56ce
Atharv upadhyay <upadhyayatharv008@gmail.con>
2025-06-28 16:04:45 +0530

    scopes

--- 01_basics/scope.js
+++ 01_basics/scope.js
@@ -2,14 +2,19 @@
 // let a= 10
 // const b= 20
 // var c = 30
+
+//var c=300 //this is global scope
+let a=300
+
 if(true){
-let a= 10
-const b= 20
-var c = 30
-}//scope
+   let a= 10   //if k andar is block scope 
+   console.log("INNER:",a);
+   const b= 20
+   //c = 30  // ye block k bahr nai jana chaiye tha but yejata h coz v is writtern in var
+}
 
+//node m scope alag h and console m jake scope alg hota h
 
-
-console.log(a);
-console.log(b);
-console.log(c);
+ console.log(a);
+//console.log(b);
+//console.log(c);//this prints c i.e. 30 which should not happen here c is var
