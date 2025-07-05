37a654c9d1230b229b99df9fcc0e2e40529a53f4
Atharv upadhyay <upadhyayatharv008@gmail.con>
2025-07-06 00:32:31 +0530

    switch

---
+++ 01_basics/switch.js
@@ -0,0 +1,30 @@
+//switchcase
+// 
+// switch (key) {
+//       case value:
+            
+//             break;
+
+//       default:
+//             break;
+// }
+const month =3
+
+switch (month) {
+      case 1:
+            console.log("january")
+            break;
+      case 2:
+            console.log("feburary")
+            break;
+      case 3:
+            console.log("march")
+            break;
+      case 1:
+            console.log("april")
+            break; // break ka kam k to snap out of the floew and stop the rest from getting executed
+
+      default:
+            console.log("default case match")
+            break;
+}
