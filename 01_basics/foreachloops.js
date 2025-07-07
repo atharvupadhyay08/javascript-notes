1bb1770d4670e8b79f16bc0b8192171c86b49ce3
Atharv upadhyay <upadhyayatharv008@gmail.con>
2025-07-07 16:38:45 +0530

    lop

---
+++ 01_basics/foreachloops.js
@@ -0,0 +1,49 @@
+const coding = ["js","ruby","java","python","cpp"]
+//higher order function k liye hota h
+
+// (callbackfn: (value: T, index: number, array: readonly T[]) => void, thisArg?: any): void
+// array.forEach(element => {
+      
+// });
+// ye for each ko bs function dena hota h baki sab isko pata hota h length etc etc
+// coding.forEach(function (val)  {
+//      console.log(val); 
+// }); // ek function jisme nam nai dena h ie callback function or for each k argument me hi function likhna h
+
+// coding.forEach( (item)=>{
+//       console.log(item);
+// })
+
+// function printMe(item){
+//       console.log(item);
+// }
+
+// coding.forEach(printMe); //not coding.forEach(printMe());
+
+// coding.forEach((item,index,arr)=>{
+//   console.log(item,index,arr);
+// })
+
+// js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+
+
+const myCoding = [
+      {langname:"javascript",
+            langfilename:"js"
+      },
+      {langname:"java",
+            langfilename:"java"
+      },
+      {langname:"python",
+            langfilename:"py"
+      },
+]
+
+myCoding.forEach((item)=>{
+
+console.log(item.langname);
+})
