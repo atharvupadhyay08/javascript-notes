f9946aadd931c8c438f622df0c35e7783bf50bce
Atharv upadhyay <upadhyayatharv008@gmail.con>
2025-07-08 13:13:18 +0530

    new

--- 01_basics/maps.js
+++ 01_basics/maps.js
@@ -24,6 +24,16 @@ map.set('IN',"India")
 // [ 'USA', 'Unites States of America' ]
 // [ 'FR', 'france' ]
 
-for (const [key,value] of map) {
-      console.log(key,':-',value);
-}
+// for (const [key,value] of map) {
+//       console.log(key,':-',value);
+// }
+
+const myNums =[1,2,3,4,5,6,7,8,9,10];
+
+// const myNewnums = myNums.map((num)=> num+10);
+// console.log(myNewnums);
+
+const newNums = myNums.map((num)=>num*10)//iska result next m jaega
+                      .map(num=>num+1)
+                      .filter((num)=> num >= 40)
+                      console.log(newNums)
